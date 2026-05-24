f91f30384dbad07d43011db6f0c74d72d34f9453
Easyseob <83761245+Soldbone@users.noreply.github.com>
2026-05-25 00:52:03 +0900

    feat: add forward() and backward() to ReLU class

--- src/activations.py
+++ src/activations.py
@@ -30,6 +30,11 @@ class ReLU:
             x와 같은 shape. x > 0인 위치만 원래 값을 유지합니다.
         """
         # TODO: x > 0 위치를 self.mask에 저장하고, 음수/0 위치는 0으로 바꾸세요.
+        self.mask = (x > 0)
+        out = x.copy()
+        out[~self.mask] = 0
+
+        return out
         raise NotImplementedError("ReLU.forward를 구현하세요.")
 
     def backward(self, dout):
@@ -41,6 +46,10 @@ class ReLU:
             ReLU 입력 x에 대한 gradient. forward 때 x <= 0이었던 위치는 0입니다.
         """
         # TODO: forward에서 저장한 self.mask를 이용해 gradient가 흐를 위치만 남기세요.
+        dout[~self.mask] = 0
+        dx = dout
+
+        return dx
         raise NotImplementedError("ReLU.backward를 구현하세요.")
 
 
