50a96b52ae6ee106e07a0e6c3f0268979ca94239
Easyseob <83761245+Soldbone@users.noreply.github.com>
2026-05-25 20:11:50 +0900

    feat(BatchNorm): add forward() and backward()

--- src/layers.py
+++ src/layers.py
@@ -84,6 +84,12 @@ class BatchNorm:
         self.running_mean = np.zeros_like(beta)
         self.running_var = np.zeros_like(beta)
         self.eps = 1e-7
+        self.batch_size = None
+        self.xc = None
+        self.xn = None
+        self.std = None
+        self.dgamma = None
+        self.dbeta = None
 
     def forward(self, x, train=True):
         """
@@ -95,6 +101,25 @@ class BatchNorm:
             정규화 후 gamma, beta가 적용된 배열
         """
         # TODO: train=True에서는 batch mean/var로 정규화하고 running 통계를 갱신하세요.
+        if train:
+            mu = np.mean(x, axis=0)
+            xc = x - mu
+            var = np.mean(xc**2, axis=0)
+            std = np.sqrt(var + self.eps)
+            xn = xc / std
+
+            self.batch_size = x.shape[0]
+            self.xc = xc
+            self.xn = xn
+            self.std = std
+            self.running_mean = self.momentum * self.running_mean + (1 - self.momentum) * mu
+            self.running_var = self.momentum * self.running_var + (1 - self.momentum) * var
+        else:
+            xc = x - self.running_mean
+            xn = xc / np.sqrt(self.running_var + self.eps)
+
+        out = self.gamma * xn + self.beta
+        return out
         # TODO: train=False에서는 running_mean/running_var를 사용하세요.
         raise NotImplementedError("BatchNorm.forward를 구현하세요.")
 
@@ -110,6 +135,18 @@ class BatchNorm:
         """
         # TODO: self.dbeta, self.dgamma, dx를 계산하세요.
         # 힌트: 먼저 dbeta와 dgamma shape가 beta/gamma와 같은지 확인합니다.
+        self.dbeta = np.sum(dout, axis=0)
+        self.dgamma = np.sum(self.xn * dout, axis=0)
+
+        dxn = self.gamma * dout
+        dxc = dxn / self.std
+        dstd = -np.sum(dxn * self.xc / (self.std**2), axis=0)
+        dvar = 0.5 * dstd / self.std
+        dxc += (2.0 / self.batch_size) * self.xc * dvar
+        dmu = np.sum(dxc, axis=0)
+        dx = dxc - dmu / self.batch_size
+
+        return dx
         raise NotImplementedError("BatchNorm.backward를 구현하세요.")
 
 
